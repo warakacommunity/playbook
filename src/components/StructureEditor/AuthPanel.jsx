033774808c6347c13b2c764c8a1e7905f9452c80
Seid Muhie Yimam <seidymam@gmail.com>
2026-05-10 05:33:51 +0200

    refactor: drop redundant React default import (new JSX transform active)

--- src/components/StructureEditor/AuthPanel.jsx
+++ src/components/StructureEditor/AuthPanel.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import { useState, useRef } from 'react';
 import { loginWithGitHub, startDeviceFlow, pollDeviceFlow } from '@site/src/utils/github';
 import styles from './index.module.css';
 
--- src/components/StructureEditor/index.jsx
+++ src/components/StructureEditor/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo, useCallback, useRef } from 'react';
+import { useState, useEffect, useMemo, useCallback, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import {
