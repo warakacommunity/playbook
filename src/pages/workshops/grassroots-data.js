c7b4589bc1aff84078afaa18783c4dff015a8b27
Seid Muhie Yimam <seidymam@gmail.com>
2026-05-21 18:39:41 +0200

    bug: fix deployment issues

--- src/pages/workshops/grassroots-data.js
+++ src/pages/workshops/grassroots-data.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { WORKSHOPS } from '@site/src/data/workshops';
-import WorkshopDetail from './WorkshopDetail';
+import WorkshopDetail from './_WorkshopDetail';
 
 export default function Page() {
   return <WorkshopDetail workshop={WORKSHOPS.find(w => w.id === 'grassroots-data')} />;
